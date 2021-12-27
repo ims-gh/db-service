e74f906a6842d2bbb73f6784ec848ca038f70234
Dzifa Hodey <dzifahodey@gmail.com>
2021-12-27 18:22:12 +0000

    updated java version pom.xml

--- src/main/java/com/ims/dbservice/DbServiceApplication.java
+++ src/main/java/com/ims/dbservice/DbServiceApplication.java
@@ -8,6 +8,9 @@ public class DbServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbServiceApplication.class, args);
+
+		System.out.println("hello");
 	}
+
 
 }
